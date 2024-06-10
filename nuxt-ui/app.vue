536674f7e2941509bdec623ce99ebb396827dfb8
oyenet1 <bowofadeoyerinde@gmail.com>
2024-06-10 10:37:42 +0100

    nuxt-ui

--- nuxt-ui/app.vue
+++ nuxt-ui/app.vue
@@ -14,6 +14,8 @@
 </UCard>
 </UContainer> -->
   <div class=" w-screen h-screen flex justify-center items-center">
-    <div class="bg-white p-4 rounded-lg shadow-sm w-[50%]"></div>
+    <div class="bg-white p-4 rounded-lg shadow-sm w-3/4">
+      <h1 class="text-4xl font-bold text-center text-blue-600 p-4">Greeting App</h1>
+    </div>
   </div>
 </template>
--- nuxt-ui/nuxt.config.ts
+++ nuxt-ui/nuxt.config.ts
@@ -1,10 +1,6 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-// export default defineNuxtConfig({
-//   devtools: { enabled: true },
-//   ssr: false,
-//   modules: ["@nuxt/ui"]
-// })
 
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   // (optional) Enable the Nuxt devtools
   devtools: { enabled: true },
